c8fbcfffa48c4229eaad92d52bd05cfc09dcc5dc
yume <ayanami_yume@qq.com>
2018-12-13 22:32:03 +0800

    最初提交，什么都没有

---
+++ src/main/java/vip/acgarea/study181213/Study181213Application.java
@@ -0,0 +1,14 @@
+package vip.acgarea.study181213;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study181213Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Study181213Application.class, args);
+    }
+
+}
+
---
+++ src/test/java/vip/acgarea/study181213/Study181213ApplicationTests.java
@@ -0,0 +1,17 @@
+package vip.acgarea.study181213;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Study181213ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
